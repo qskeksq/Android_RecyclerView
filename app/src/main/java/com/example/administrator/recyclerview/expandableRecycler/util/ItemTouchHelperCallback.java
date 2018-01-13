88ffd68f4f5bef0f299609badc5c1e88a09446bd
qskeksq <qskeksq@gmail.com>
2018-01-13 14:32:56 +0900

    Update ItemTouchHelperCallback.java

--- app/src/main/java/com/example/administrator/recyclerview/expandableRecycler/util/ItemTouchHelperCallback.java
+++ app/src/main/java/com/example/administrator/recyclerview/expandableRecycler/util/ItemTouchHelperCallback.java
@@ -15,6 +15,7 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
         this.listener = listener;
     }
 
+     // 각 view에서 어떤 user action이 가능한지 정의
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         int dragFlag = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
@@ -22,11 +23,13 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
         return makeMovementFlags(dragFlag, swipeFlag);
     }
 
+     // user가 item을 drag할 때, ItemTouchHelper가 onMove()를 호출
     @Override
     public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
         return listener.onItemMove(viewHolder.getAdapterPosition(), target.getAdapterPosition());
     }
 
+     // view가 swipe될 때, ItemTouchHelper는 뷰가 사라질때까지 animate한 후, onSwiped()를 호출
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         listener.onRemove(viewHolder.getAdapterPosition());
